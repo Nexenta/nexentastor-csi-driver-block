bd6ee95bfb46e2bf3ab2c8e5372adf705a87bb83
Alexey Khodos <akhodos@tintri.com>
2022-01-14 20:08:19 +0000

    ES-525: remove all hardcoded values for insecureSkipVerify from the code

--- pkg/driver/nodeServer.go
+++ pkg/driver/nodeServer.go
@@ -76,7 +76,7 @@ func (s *NodeServer) refreshConfig(secret string) error {
                 Username:           cfg.Username,
                 Password:           cfg.Password,
                 Log:                s.log,
-                InsecureSkipVerify: true, //TODO move to config
+                InsecureSkipVerify: *cfg.InsecureSkipVerify,
             })
             if err != nil {
                 return fmt.Errorf("Cannot create NexentaStor resolver: %s", err)
@@ -1244,7 +1244,7 @@ func NewNodeServer(driver *Driver) (*NodeServer, error) {
             Username:           cfg.Username,
             Password:           cfg.Password,
             Log:                l,
-            InsecureSkipVerify: true, //TODO move to config
+            InsecureSkipVerify: *cfg.InsecureSkipVerify,
         })
         if err != nil {
             return nil, fmt.Errorf("Cannot create NexentaStor resolver: %s", err)
